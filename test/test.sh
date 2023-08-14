7b153505405ce6e558ddb76b3a51d99259cfa33e
Nils Stein <social.nst@mailbox.org>
2023-08-14 16:26:56 +0200

    Fix test

--- test/test.sh
+++ test/test.sh
@@ -21,7 +21,7 @@ echo ""
 echo "Waiting for Stack to become ready..."
 echo "------------------------------------------------------------"
 COUNTER=1
-while [ ${COUNTER} -le 12 ]; do
+while [ ${COUNTER} -le 24 ]; do
 
 if $(docker logs mautrix-signal 2>&1 | grep -q "Connected to signald") && \
     $(docker logs synapse 2>&1 | grep -q "No more background updates to do. Unscheduling background update task"); then
@@ -31,8 +31,8 @@ else
     echo "..."
     sleep 10
     COUNTER=$[$COUNTER+1]
-    if [ ${COUNTER} -gt 12 ]; then
-        echo "Stack not ready after 120 seconds, breaking wait loop..."
+    if [ ${COUNTER} -gt 24 ]; then
+        echo "Stack not ready after 240 seconds, breaking wait loop..."
     fi
 fi
 done
