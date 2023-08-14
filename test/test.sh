739f5b1656323b324f6a5420903dfc372c4ea0b7
Nils Stein <social.nst@mailbox.org>
2023-08-14 14:49:36 +0200

    formatting

--- test/test.sh
+++ test/test.sh
@@ -22,7 +22,8 @@ echo "------------------------------------------------------------"
 COUNTER=1
 while [ ${COUNTER} -le 12 ]; do
 
-if $(docker logs mautrix-signal 2>&1 | grep -q "Connected to signald") && $(docker logs synapse 2>&1 | grep -q "No more background updates to do. Unscheduling background update task"); then
+if $(docker logs mautrix-signal 2>&1 | grep -q "Connected to signald") && \
+    $(docker logs synapse 2>&1 | grep -q "No more background updates to do. Unscheduling background update task"); then
     echo "Stack is ready! breaking wait loop..."
     break 
 else
@@ -35,15 +36,16 @@ else
 fi
 done
 
-# Install tools needed for inspect
+echo ""
+echo "Prepare"
+echo "------------------------------------------------------------"
+echo ""
 docker exec -u 0 mautrix-signal apk add --no-cache procps || STATUS=1
 
 echo ""
 echo "Test"
 echo "------------------------------------------------------------"
-echo ""
 inspec exec ./system/host || STATUS=1
-echo ""
 inspec exec ./system/container -t docker://mautrix-signal || STATUS=1
 
 echo ""
@@ -54,7 +56,14 @@ docker compose down --remove-orphans --volumes
 echo ""
 git clean -fxd
 
-if [ $STATUS -eq 1 ]; then
+echo ""
+echo "Result"
+echo "------------------------------------------------------------"
+if [ $STATUS -eq 0 ]; then
+    echo ""
+    echo "SUCSSES!"
+else
+    echo ""
     echo "FAILED!"
     exit 1
 fi
