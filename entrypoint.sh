cafa9184fd03dd7ae93264c99110fe5b0cc9cdbe
Nils Stein <social.nst@mailbox.org>
2023-08-15 12:11:21 +0200

    Use non root user

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-exec su-exec $UID:$GID /bin/signald -d /signald -s /var/run/signald.sock &
+exec su-exec $UID:$GID /bin/signald -d /signald -s /var/run/signald/signald.sock &
 
 source /opt/mautrix-signal/docker-run.sh
